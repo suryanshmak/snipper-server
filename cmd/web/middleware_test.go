b7c02b3f7f81290d685875a7a6c3178e92b445c6
Suryansh Makharia <makhariasuryansh7@gmail.com>
2022-01-24 18:19:47 +0530

    unit testing middlewares

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	next := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("OK"))
+	})
+
+	secureHeaders(next).ServeHTTP(rr, r)
+
+	rs := rr.Result()
+
+	frameOptions := rs.Header.Get("X-Frame-Options")
+	if frameOptions != "deny" {
+		t.Errorf("want deny; got %s", frameOptions)
+	}
+
+	xssOptions := rs.Header.Get("X-XSS-Protection")
+	if xssOptions != "1; mode=block" {
+		t.Errorf("want 1; mode=block; got %s", xssOptions)
+	}
+
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
